0392ce5be3cad2450e6c5b7fcdffbc25ec66bfcd
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2025-10-08 11:56:38 +0200

    fix: remove unused struct that fails linting checks

--- src/types.rs
+++ src/types.rs
@@ -15,33 +15,6 @@ use crate::debug_images::Image;
 use crate::nodetree::Node;
 use crate::sample::v1::Measurement;
 use crate::sample::SampleError;
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ChunkMeasurement {
-    unit: MeasurementUnit,
-    values: Vec<ChunkMeasurementValue>,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
-pub enum MeasurementUnit {
-    #[serde(alias = "ns")]
-    Nanosecond,
-    #[serde(alias = "hz")]
-    Hertz,
-    Byte,
-    Percent,
-    #[serde(alias = "nj")]
-    Nanojoule,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ChunkMeasurementValue {
-    // UNIX timestamp in seconds as a float
-    timestamp: f64,
-
-    value: f64,
-}
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct ClientSDK {
     pub name: String,
